a1101a1ac2481325bcfaacc1c0ba68cb5fa07d55
JonaThanPabonP <jpaco95@gmail.com>
2021-10-27 14:39:49 -0500

    Clase lunes con resolución de ejercicios.

---
+++ Clases/s1_c4-ejercicios.js
@@ -0,0 +1,29 @@
+var estudiantes = ['Mariana', 'Alexander', 'Pedro', 'Carolina'];
+var numero = estudiantes.length;
+
+while (numero != 0) {
+    numero--;
+    
+    console.log(estudiantes[numero-3]);
+}
+
+// let sumaPares = 0;
+// let sumaImpares = 0;
+
+// for (let i = 0; i <= 578934; i++) {
+//     if (i%2 == 0) {
+//         sumaPares += i; 
+//     } else{
+//         sumaImpares += i;
+//     }
+// }
+// console.log(sumaPares);
+// console.log(sumaImpares);
+
+// for (let i = 1; i < 20; i++) {
+//     let linea = '';
+//     for (let j = 1; j <= i; j++) {
+//         linea+=' '+j;
+//     }
+//     console.log(linea);
+// }
